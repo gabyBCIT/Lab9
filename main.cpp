c1be6457eb725358bbaa0003b9b3ca166b4e7c14
Gaby Lamoureux <glamoureux@my.bcit.ca>
2018-11-10 00:03:25 -0800

    Adding addition.

--- main.cpp
+++ main.cpp
@@ -2,21 +2,29 @@
 #include <vector>
 #include <set>
 #include <deque>
-#include <multiset>
 #include <map>
+#include <random>
 
 using namespace std;
-#define RANDS 20
 
 int main() {
-    vector<int> vex;
+    const int rands = 20;
+    default_random_engine generator(time(0));
+    uniform_real_distribution<double> distribution(0.00, 1000.00);
 
-    for (int i = 0; i < RANDS; i++ ) {
-        vex[i] = rand();
+    vector<double> vex;
+
+    // Creates random doubles
+    for (int i = 0; i < rands; i++ ) {
+        vex.push_back(distribution(generator));
+        cout << vex[i] << "\t";
     }
+    cout << "\n";
 
-
-
+    // Shows iterator adding to itself
+    for(vector<double>::iterator rato = vex.begin(); rato != vex.end(); rato++){
+        cout << *rato + *rato << "\t";
+    }
     return 0;
 }
 
