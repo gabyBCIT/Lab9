339d04e3ecab60537120494e862889515f827bb8
Gaby Lamoureux <glamoureux@my.bcit.ca>
2018-11-09 23:45:09 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <deque>
+#include <multiset>
+#include <map>
+
+using namespace std;
+#define RANDS 20
+
+int main() {
+    vector<int> vex;
+
+    for (int i = 0; i < RANDS; i++ ) {
+        vex[i] = rand();
+    }
+
+
+
+    return 0;
+}
+
